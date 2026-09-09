d5308d5979e4a82b38e28857004678700309a73d
znbwzer <znbwzer2020@gmail.com>
2026-09-09 23:35:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,4 +12,5 @@ def health():
     return {'status': 'healthy'}
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
